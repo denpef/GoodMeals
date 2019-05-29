d6766aa5f7ff28edda81168d16955bf1024aaf66
Denis Efimov <denis.efimov@btsdigital.kz>
2019-05-30 02:30:42 +0600

    add tests to ingredients list

--- src/GoodMealsTests/IngredientsListViewModelTests.swift
+++ src/GoodMealsTests/IngredientsListViewModelTests.swift
@@ -6,20 +6,47 @@ import RxCocoa
 
 class IngredientsListViewModelTests: XCTestCase {
 
-    var sut: IngredientsListViewModel?
+    // MARK: - Properties
+    
     let service = IngredientsServiceTypeMock()
     let router = IngredientsListRouterTypeMock()
+    
+    var sut: IngredientsListViewModel?
+    var ingredient = Ingredient(name: "Mock")
 
+    // MARK: - Override test methods
+    
     override func setUp() {
+        ingredient.id = "0"
         service.subscribeCollectionSubscriberCallsCount = 0
-        service.allReturnValue = []
+        service.allReturnValue = [ingredient]
         sut = IngredientsListViewModel(ingredientsService: service)
         sut?.router = router
+        router.navigateToIngredientIngredientIdCallsCount = 0
     }
     
+    // MARK: - test functions
+    
     func testWhenInitialized() {
-        XCTAssertNotNil(sut?.items)
-        XCTAssertTrue(service.subscribeCollectionSubscriberCalled)
+        XCTAssertNotNil(sut?.items, "items is nil by default")
+        XCTAssertTrue(service.subscribeCollectionSubscriberCalled, "viewModel not subscribed on persistance updates")
         XCTAssertEqual(service.subscribeCollectionSubscriberCallsCount, 1)
+    }
+    
+    func testSelectItem() {
+        let selectItem = sut?.selectItem.asObserver()
+        selectItem?.onNext(ingredient)
+        XCTAssertTrue(router.navigateToIngredientIngredientIdCalled, "routing is failed")
+        XCTAssertEqual(router.navigateToIngredientIngredientIdCallsCount, 1, "routing is failed")
+        XCTAssertEqual(router.navigateToIngredientIngredientIdReceivedIngredientId,
+                       ingredient.id,
+                       "incorrect id when navigation is beginnig")
+    }
+    
+    func testAddNewItem() {
+        let addNewItem = sut?.addNewItem.asObserver()
+        addNewItem?.onNext(())
+        XCTAssertTrue(router.navigateToIngredientIngredientIdCalled, "routing is failed")
+        XCTAssertEqual(router.navigateToIngredientIngredientIdCallsCount, 1, "routing is failed")
     }
 }
