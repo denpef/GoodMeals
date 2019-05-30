f093915bc4cabc4edfc15bd16c050f075446734b
Denis Efimov <denpef@gmail.com>
2019-05-30 22:35:54 +0600

    recipe list tests

--- src/Generated/AutoMockable.swift
+++ src/Generated/AutoMockable.swift
@@ -130,3 +130,121 @@ class IngredientsServiceTypeMock: IngredientsServiceType {
         clearAllClosure?()
     }
 }
+class RecipesListRouterTypeMock: RecipesListRouterType {
+    //MARK: - navigateToRecipe
+
+    var navigateToRecipeRecipeIdCallsCount = 0
+    var navigateToRecipeRecipeIdCalled: Bool {
+        return navigateToRecipeRecipeIdCallsCount > 0
+    }
+    var navigateToRecipeRecipeIdReceivedRecipeId: String?
+    var navigateToRecipeRecipeIdReceivedInvocations: [String?] = []
+    var navigateToRecipeRecipeIdClosure: ((String?) -> Void)?
+    func navigateToRecipe(recipeId: String?) {
+        navigateToRecipeRecipeIdCallsCount += 1
+        navigateToRecipeRecipeIdReceivedRecipeId = recipeId
+        navigateToRecipeRecipeIdReceivedInvocations.append(recipeId)
+        navigateToRecipeRecipeIdClosure?(recipeId)
+    }
+}
+class RecipesServiceTypeMock: RecipesServiceType {
+    //MARK: - getModel
+
+    var getModelByCallsCount = 0
+    var getModelByCalled: Bool {
+        return getModelByCallsCount > 0
+    }
+    var getModelByReceivedId: String?
+    var getModelByReceivedInvocations: [String] = []
+    var getModelByReturnValue: Recipe?
+    var getModelByClosure: ((String) -> Recipe?)?
+    func getModel(by id: String) -> Recipe? {
+        getModelByCallsCount += 1
+        getModelByReceivedId = id
+        getModelByReceivedInvocations.append(id)
+        return getModelByClosure.map({ $0(id) }) ?? getModelByReturnValue
+    }
+    //MARK: - add
+
+    var addCallsCount = 0
+    var addCalled: Bool {
+        return addCallsCount > 0
+    }
+    var addReceivedRecipe: Recipe?
+    var addReceivedInvocations: [Recipe] = []
+    var addClosure: ((Recipe) -> Void)?
+    func add(_ Recipe: Recipe) {
+        addCallsCount += 1
+        addReceivedRecipe = Recipe
+        addReceivedInvocations.append(Recipe)
+        addClosure?(Recipe)
+    }
+    //MARK: - remove
+
+    var removeCallsCount = 0
+    var removeCalled: Bool {
+        return removeCallsCount > 0
+    }
+    var removeReceivedRecipe: Recipe?
+    var removeReceivedInvocations: [Recipe] = []
+    var removeClosure: ((Recipe) -> Void)?
+    func remove(_ Recipe: Recipe) {
+        removeCallsCount += 1
+        removeReceivedRecipe = Recipe
+        removeReceivedInvocations.append(Recipe)
+        removeClosure?(Recipe)
+    }
+    //MARK: - update
+
+    var updateCallsCount = 0
+    var updateCalled: Bool {
+        return updateCallsCount > 0
+    }
+    var updateReceivedRecipe: Recipe?
+    var updateReceivedInvocations: [Recipe] = []
+    var updateClosure: ((Recipe) -> Void)?
+    func update(_ Recipe: Recipe) {
+        updateCallsCount += 1
+        updateReceivedRecipe = Recipe
+        updateReceivedInvocations.append(Recipe)
+        updateClosure?(Recipe)
+    }
+    //MARK: - all
+
+    var allCallsCount = 0
+    var allCalled: Bool {
+        return allCallsCount > 0
+    }
+    var allReturnValue: [Recipe]!
+    var allClosure: (() -> [Recipe])?
+    func all() -> [Recipe] {
+        allCallsCount += 1
+        return allClosure.map({ $0() }) ?? allReturnValue
+    }
+    //MARK: - subscribeCollection
+
+    var subscribeCollectionSubscriberCallsCount = 0
+    var subscribeCollectionSubscriberCalled: Bool {
+        return subscribeCollectionSubscriberCallsCount > 0
+    }
+    var subscribeCollectionSubscriberReceivedSubscriber: PersistenceNotificationOutput?
+    var subscribeCollectionSubscriberReceivedInvocations: [PersistenceNotificationOutput] = []
+    var subscribeCollectionSubscriberClosure: ((PersistenceNotificationOutput) -> Void)?
+    func subscribeCollection(subscriber: PersistenceNotificationOutput) {
+        subscribeCollectionSubscriberCallsCount += 1
+        subscribeCollectionSubscriberReceivedSubscriber = subscriber
+        subscribeCollectionSubscriberReceivedInvocations.append(subscriber)
+        subscribeCollectionSubscriberClosure?(subscriber)
+    }
+    //MARK: - clearAll
+
+    var clearAllCallsCount = 0
+    var clearAllCalled: Bool {
+        return clearAllCallsCount > 0
+    }
+    var clearAllClosure: (() -> Void)?
+    func clearAll() {
+        clearAllCallsCount += 1
+        clearAllClosure?()
+    }
+}
---
+++ src/GoodMealsTests/RecipesListViewModelTests.swift
@@ -0,0 +1,50 @@
+import XCTest
+import RxSwift
+import RxCocoa
+
+@testable import GoodMeals
+
+class RecipesListViewModelTests: XCTestCase {
+    
+    // MARK: - Properties
+    
+    let service = RecipesServiceTypeMock()
+    let router = RecipesListRouterTypeMock()
+    
+    var sut: RecipesListViewModel?
+    var recipe = Recipe(name: "Mock")
+    
+    // MARK: - Override test methods
+    
+    override func setUp() {
+        recipe.id = "0"
+        service.subscribeCollectionSubscriberCallsCount = 0
+        service.allReturnValue = [recipe]
+        sut = RecipesListViewModel(recipesService: service)
+        sut?.router = router
+        router.navigateToRecipeRecipeIdCallsCount = 0
+    }
+    
+    // MARK: - test functions
+    
+    func testWhenInitialized() {
+        XCTAssertNotNil(sut?.items, "items is nil by default")
+        XCTAssertTrue(service.subscribeCollectionSubscriberCalled, "viewModel not subscribed on persistance updates")
+        XCTAssertEqual(service.subscribeCollectionSubscriberCallsCount, 1)
+    }
+    
+    func testSelectItem() {
+        let selectItem = sut?.selectItem.asObserver()
+        selectItem?.onNext(recipe)
+        XCTAssertTrue(router.navigateToRecipeRecipeIdCalled, "routing is failed")
+        XCTAssertEqual(router.navigateToRecipeRecipeIdCallsCount, 1, "routing is failed")
+        XCTAssertEqual(router.navigateToRecipeRecipeIdReceivedRecipeId, recipe.id, "incorrect id when navigation is beginnig")
+    }
+    
+    func testAddNewItem() {
+        let addNewItem = sut?.addNewItem.asObserver()
+        addNewItem?.onNext(())
+        XCTAssertTrue(router.navigateToRecipeRecipeIdCalled, "routing is failed")
+        XCTAssertEqual(router.navigateToRecipeRecipeIdCallsCount, 1, "routing is failed")
+    }
+}
