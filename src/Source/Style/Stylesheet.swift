fc8a89c2226c31673746458380e42fe624e7aaf9
Denis Efimov <denpef@gmail.com>
2019-07-08 01:45:01 +0600

    shopping styles

--- src/Source/Style/Stylesheet.swift
+++ src/Source/Style/Stylesheet.swift
@@ -30,4 +30,59 @@ enum Stylesheet {
             $0.spacing = 6
         }
     }
+
+    enum ShoppingList {
+        static let tableView = Style<UITableView> {
+            $0.rowHeight = 60
+            $0.allowsSelection = false
+            $0.backgroundColor = .clear
+            $0.separatorStyle = .none
+        }
+    }
+
+    enum GroceryCell {
+        static let markButtonMarked = Style<UIButton> {
+            $0.setImage(Asset.image.image, tintColor: .green)
+            $0.tintColor = UIColor.Common.ghostWhite
+        }
+
+        static let deleteButtonMarked = Style<UIButton> {
+            $0.setImage(Asset.cancel.image, tintColor: UIColor.Common.deleteItem)
+            $0.tintColor = UIColor.Common.ghostWhite
+        }
+
+        static let titleLabelMarked = Style<UILabel> {
+            $0.textAlignment = .left
+            $0.textColor = UIColor.Common.markedText
+            $0.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        }
+
+        static let amountLabelMarked = Style<UILabel> {
+            $0.textAlignment = .right
+            $0.textColor = UIColor.Common.markedText
+            $0.font = UIFont.systemFont(ofSize: 12, weight: .light)
+        }
+
+        static let markButtonUnmarked = Style<UIButton> {
+            $0.setImage(Asset.image.image, tintColor: .green)
+            $0.tintColor = UIColor.Common.controlText
+        }
+
+        static let deleteButtonUnmarked = Style<UIButton> {
+            $0.setImage(Asset.cancel.image, tintColor: UIColor.Common.deleteItem)
+            $0.tintColor = UIColor.Common.deleteItem
+        }
+
+        static let titleLabelUnmarked = Style<UILabel> {
+            $0.textAlignment = .left
+            $0.textColor = UIColor.Common.controlBackground
+            $0.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        }
+
+        static let amountLabelUnmarked = Style<UILabel> {
+            $0.textAlignment = .right
+            $0.textColor = UIColor.Common.controlBackground
+            $0.font = UIFont.systemFont(ofSize: 12, weight: .light)
+        }
+    }
 }
