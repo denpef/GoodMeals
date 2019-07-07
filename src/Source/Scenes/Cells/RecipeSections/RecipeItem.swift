44eee9441cf56d53c0c43e36cd3ac5cf60ba5df8
Denis Efimov <denpef@gmail.com>
2019-07-08 01:46:44 +0600

    equatable

--- src/Source/Scenes/Cells/RecipeSections/RecipeItem.swift
+++ src/Source/Scenes/Cells/RecipeSections/RecipeItem.swift
@@ -2,3 +2,16 @@ enum RecipeItem {
     case ingredientItem(ingredient: IngredientAmount)
     case servingItem(calorific: Int, timeForPreparing: String)
 }
+
+extension RecipeItem: Equatable {
+    static func == (lhs: RecipeItem, rhs: RecipeItem) -> Bool {
+        switch (lhs, rhs) {
+        case let (.ingredientItem(ingredientLhs), .ingredientItem(ingredientRhs)):
+            return ingredientLhs == ingredientRhs
+        case (let .servingItem(calorificLhs, timeForPreparingLhs), let .servingItem(calorificRhs, timeForPreparingRhs)):
+            return calorificLhs == calorificRhs && timeForPreparingLhs == timeForPreparingRhs
+        default:
+            return false
+        }
+    }
+}
