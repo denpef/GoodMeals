4a94f21b086722800734b2721a714f7aca706651
Denis Efimov <denpef@gmail.com>
2019-07-08 03:56:39 +0600

    documentation

--- src/Source/Scenes/Recipe/RecipeViewModel.swift
+++ src/Source/Scenes/Recipe/RecipeViewModel.swift
@@ -4,19 +4,19 @@ import RxSwift
 class RecipeViewModel {
     /// Image url adress
     let image: Observable<String>
-    
+
     /// Title of screen
     let title: Driver<String>
-    
+
     /// Recipe info and action items (add to shopping list) plus list of ingredients
     let items: Driver<[RecipeSection]>
-    
+
     /// Initial recipe value
     var recipe: Observable<Recipe>
 
     /// Count of serving for calculation ingredients amount
     let serving = BehaviorRelay<Int>(value: 2)
-    
+
     /// Handle control action add all ingredients to shopping list
     let addToShoppingListAction = PublishRelay<Void>()
 
@@ -43,27 +43,27 @@ class RecipeViewModel {
 
         items = recipe.map { recipe -> [RecipeSection] in
             var recipeItems = [RecipeItem]()
-            
+
             recipeItems.append(.servingItem(calorific: recipe.calorific,
                                             timeForPreparing: recipe.timeForPreparing))
-            
+
             recipe.ingredients.forEach {
                 recipeItems.append(.ingredientItem(ingredient: $0))
             }
-            
+
             return [RecipeSection(items: recipeItems)]
         }.asDriver(onErrorJustReturn: [])
 
         Observable.combineLatest(addToShoppingListAction, recipe, serving)
             .flatMapLatest { _, recipe, serving -> Observable<[GroceryItem]> in
                 var items = [GroceryItem]()
-                
+
                 recipe.ingredients.forEach {
                     items.append(GroceryItem(ingredient: $0.ingredient,
                                              amount: $0.amount * serving,
                                              marked: false))
                 }
-                
+
                 return Observable.from(optional: items)
             }.subscribe(onNext: { groceryItems in
                 groceryItems.forEach {
